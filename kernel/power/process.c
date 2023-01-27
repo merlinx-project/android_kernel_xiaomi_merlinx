adfd3695bf49f9d26592ffa348ea98fe2851274a
Gian <gianpaoloestacio5@gmail.com>
2023-01-28 01:17:39 +0800

    kernel: power: Nuke some logspams
    Signed-off-by: Gian <gianpaoloestacio5@gmail.com>
    Signed-off-by: tejas101k <tejassingh649@rediffmail.com>

--- kernel/power/process.c
+++ kernel/power/process.c
@@ -143,7 +143,6 @@ int freeze_processes(void)
 		__usermodehelper_set_disable_depth(UMH_DISABLED);
 		pr_cont("done.");
 	}
-	pr_cont("\n");
 	BUG_ON(in_atomic());
 
 	/*
@@ -179,7 +178,6 @@ int freeze_kernel_threads(void)
 	if (!error)
 		pr_cont("done.");
 
-	pr_cont("\n");
 	BUG_ON(in_atomic());
 
 	if (error)
