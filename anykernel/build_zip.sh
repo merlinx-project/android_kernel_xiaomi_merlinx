77258ea186ff5bc7276927c8fd53e77657309a71
areyoudeveloper <kmd36rk@gmail.com>
2025-01-15 12:10:12 +0700

    buider: initial import

---
+++ anykernel/build_zip.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+ak_dir=anykernel/any
+image=out/arch/arm64/boot/Image.gz-dtb
+dtbimg=out/arch/arm64/boot/dtbo.img
+pre=` cat out/.config | grep Linux | cut -f 3 -d " "  `
+fmt=`date +%d\.%m\.%y_%r | sed 's/:/./g;s/ PM/.PM/g;s/ AM/.AM/g;s/ IST//g'`
+
+cp -rf $image $ak_dir/
+cp -rf $dtbimg $ak_dir/
+cd $ak_dir
+
+zip -r9 QuartzCrystal-4.14-A11-A13_${model}_${fmt}.zip *
+mv *.zip ../../
+cd ../../
